a96d42373d952dc9eae30a05b35d53f1c586dd6b
Kim Dany <sgoiboy@gmail.com>
2020-07-22 11:24:16 +0900

    [UI] basefragment 생성 및 기존 fragment 들 basefragment 상속받게 (#4)

---
+++ app/src/main/java/com/example/memorygame/base/BaseFragment.kt
@@ -0,0 +1,34 @@
+package com.example.memorygame.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VBD : ViewDataBinding>(
+  @LayoutRes private val layoutResId: Int
+) : Fragment() {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? = DataBindingUtil.inflate<VBD>(
+    inflater,
+    layoutResId,
+    container,
+    false
+  ).run {
+
+    bindingViewData()
+    setEventListener()
+
+    root
+  }
+
+  protected abstract fun VBD.bindingViewData()
+  protected abstract fun VBD.setEventListener()
+}
---
+++ app/src/main/java/com/example/memorygame/view/dest/RecordFragment.kt
@@ -0,0 +1,21 @@
+package com.example.memorygame.view.dest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.memorygame.R
+import com.example.memorygame.base.BaseFragment
+import com.example.memorygame.databinding.FragmentRecordBinding
+
+class RecordFragment: BaseFragment<FragmentRecordBinding>(
+  R.layout.fragment_record
+) {
+  override fun FragmentRecordBinding.bindingViewData() {
+  }
+
+  override fun FragmentRecordBinding.setEventListener() {
+  }
+}
